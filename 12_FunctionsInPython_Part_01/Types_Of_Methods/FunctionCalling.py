bc650dc2a803babb7bfe5c6f533955bedf90ac88
vivek <vivek.sirsath1@gmail.com>
2025-10-12 13:57:24 +0530

    FunctionCalling.py added

---
+++ 12_FunctionsInPython_Part_01/Types_Of_Methods/FunctionCalling.py
@@ -0,0 +1,18 @@
+def sample_function():
+    print("Hello Python")
+    print("Hello Python")
+    print("Hello Python")
+    print("Hello Python")
+    print("Hello Python")
+
+"""
+# Calling function using for loop to print the statement multiple times.
+# Instead of calling a function multiple times like
+sample_function()
+sample_function()
+sample_function()
+"""
+
+for x in range(1,21):
+    sample_function()
+
